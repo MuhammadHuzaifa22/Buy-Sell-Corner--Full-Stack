a7a6885437a64959177843aba33151b17a97bb10
MuhammadHuzaifa22 <huzaifafurqan22@gmail.com>
2024-08-11 09:54:22 +0500

    20% work completed

--- app.js
+++ app.js
@@ -1,13 +1,76 @@
-// ., Call Variables
+import { signInWithEmailAndPassword ,GoogleAuthProvider,signInWithPopup} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { collection, addDoc, setDoc, doc } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+import { auth, db } from './config.js';
 
+// ., Call Variables
 const form = document.querySelector('form');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
+const toast = document.querySelector('.toast-msg');
+const googleBtn = document.getElementById('google-btn');
+const provider = new GoogleAuthProvider();
+toast.style.display = 'none';
 
-
-form.addEventListener('submit',(event)=>{
+// ., Form Button Function
+form.addEventListener('submit', (event) => {
     event.preventDefault();
     console.log(email.value);
     console.log(password.value);
+    if ((email.value === '' || email.value === null) && (password.value === '' || password.value === null)) {
+        alert(`Please fill input fields`);
+        return
+    } else if ((email.value !== '' || email.value !== null) && (password.value === '' || password.value === null)) {
+        alert(`Please fill -Password- field`);
+        return
+    } else if ((email.value === '' || email.value === null) && (password.value !== '' || password.value !== null)) {
+        alert(`Please fill -Email- field`);
+        return
+    } else if (email.value === '' || email.value === null) {
+        alert(`Please fill -Email- field`);
+        return
+    } else if (password.value === '' || password.value === null) {
+        alert(`Please fill -Password- field`);
+        return
+    }
+    // ., Sign In With Email And Password
+    signInWithEmailAndPassword(auth, email.value, password.value)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log("🚀 ~ .then ~ user:", user);
+            toast.style.display = 'block'
+            toast.innerHTML = `<div class="toast toast-top toast-center mt-[70px]">
+  <div class="alert alert-success">
+    <span class="text-white text-sm xs:text-md sm:text-lg md:text-xl lg:text-2xl">Log in Successfull</span>
+   </div>
+       </div>`
+            setTimeout(() => {
+                toast.style.display = 'none';
+                window.location = 'index.html'
+            }, 900);
+ form.reset();
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(errorMessage);
+        });
+})
+
+
+// ., Google Button Function
+googleBtn.addEventListener('click',()=>{
+    signInWithPopup(auth, provider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      console.log(user);
+    }).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.customData.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      alert(errorMessage);
+    });
 
 })
--- register.js
+++ register.js
@@ -1,55 +1,110 @@
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-import { auth } from './config.js';
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { setDoc, doc } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+import { getDownloadURL, uploadBytes, ref } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
 
-// ., Call Variables
+import { auth, db, storage } from './config.js';
+
+// Call Variables
 const form = document.querySelector('form');
 const firstName = document.getElementById('firstname');
 const lastName = document.getElementById('lastname');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const userPhoto = document.getElementById('user-photo');
+const msgRegistered = document.getElementById('msg-registered');
+const myModal = document.getElementById('my_modal_3');
+const userProfileImage = document.getElementById('user-profile-image');
+const registerEmailMessage = document.getElementById('register-email-message');
+const crossButton = document.getElementById('cross-button');
 
-// .,Use  Regix
+// Regex
 const firstNameRegex = /^[A-Z][a-zA-Z]{3,9}$/;
+const lastNameRegex = /^[A-Z][a-zA-Z]{3,9}$/;
 const passwordRegex = /^[A-Z](?=.*\d)[A-Za-z\d]{5,9}$/;
-// ., Form Button Function
-form.addEventListener('submit', (event) => {
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+// Form Button Function
+form.addEventListener('submit', async (event) => {
     event.preventDefault();
-    if(firstName.value === '' || firstName.value === null){
-        alert(`Please fill -First Name- field`)
-    }else{
-        
+
+    
+    if (firstName.value === '' || lastName.value === '' || email.value === '' || password.value === '' || userPhoto.files.length === 0) {
+        alert('Please fill all fields and upload a photo.');
+        return;
     }
-    if (firstNameRegex.test(firstName.value)) {
 
-        if (passwordRegex.test(password.value)) {
-
-            console.log()
-            console.log(firstName.value);
-            console.log(lastName.value)
-            console.log(email.value);
-            console.log(password.value);
-            console.log(userPhoto.files[0])
-
-            createUserWithEmailAndPassword(auth, email.value, password.value)
-                .then((userCredential) => {
-                    const user = userCredential.user;
-                    console.log(user);
-                })
-                .catch((error) => {
-                    const errorCode = error.code;
-                    const errorMessage = error.message;
-                    alert(errorMessage);
-                });
-        } else {
-            alert("Your password must:\n- Start with an uppercase letter\n- Be between 6 and 10 characters long\n- Include at least one number");
-
-        }
-
-    } else {
+    if (!firstNameRegex.test(firstName.value)) {
         alert("Your first name must:\n- Start with an uppercase letter\n- Be between 4 and 10 characters long\n- Only contain letters (no numbers or special characters)");
-
+        return;
     }
 
+    if (!lastNameRegex.test(lastName.value)) {
+        alert("Your last name must:\n- Start with an uppercase letter\n- Be between 4 and 10 characters long\n- Only contain letters (no numbers or special characters)");
+        return;
+    }
 
-})
+    if (!emailRegex.test(email.value)) {
+        alert("Invalid email format. Please make sure your email follows these rules:\n\n" +
+            "1. Contains no spaces.\n" +
+            "2. Contains one '@' symbol.\n" +
+            "3. Has text before and after the '@'.\n" +
+            "4. Contains a '.' after the '@' and some text after the '.'.");
+        return;
+    }
+
+    if (!passwordRegex.test(password.value)) {
+        alert("Your password must:\n- Start with an uppercase letter\n- Be between 6 and 10 characters long\n- Include at least one number");
+        return;
+    }
+
+    try {
+
+        const userCredential = await createUserWithEmailAndPassword(auth, email.value, password.value);
+        const user = userCredential.user;
+        const file = userPhoto.files[0];
+        const storageRef = ref(storage, `userPhotos/${user.uid}/${file.name}`);
+        const uploadResult = await uploadBytes(storageRef, file);
+        const photoURL = await getDownloadURL(storageRef);
+        console.log('Photo URL:', photoURL);
+        userProfileImage.src = photoURL;
+
+        await setDoc(doc(db, 'users', user.uid), {
+            uid: user.uid,
+            email: user.email,
+            userPhoto: photoURL,
+            firstName: firstName.value,
+            lastName: lastName.value
+        });
+
+
+        msgRegistered.innerHTML = `${firstName.value} ${lastName.value}`;
+        registerEmailMessage.innerHTML = `<span class="font-light xs:font-normal sm:font-medium md:font-semibold lg:font-bold text-teal-600">You are registered</span> with this email <b>${email.value}</b>`;
+        myModal.showModal();
+
+       
+       firstName.value = '';
+       lastName.value = '';
+       email.value = '';
+       password.value = '';
+        userPhoto.value = ''; 
+        crossButton.addEventListener('click', () => {
+            window.location = 'login.html';
+        });
+
+    } catch (error) {
+        console.error('Error registering user:', error.message);
+        alert(`Error: ${error.message}`);
+    }
+});
+
+
+async function showUrl(file) {
+    const storageRef = ref(storage, email.value);
+    try {
+        const uploadImg = await uploadBytes(storageRef, file);
+        const url = await getDownloadURL(storageRef);
+        return url;
+    } catch (error) {
+        console.error('Error uploading file:', error);
+    }
+}
